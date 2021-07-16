089b219a3d0441084d85b8a0f806bbbd658f6c21
Sofiia Biletska <sofiyabiletska@gmail.com>
2021-07-16 11:06:59 +0300

    updated

--- my-first-app/src/app/get-data.service.ts
+++ my-first-app/src/app/get-data.service.ts
@@ -9,12 +9,12 @@ export class GetDataService {
   constructor(private http: HttpClient) { }
   signIn(ulogin:string, upassword:string) {
     let userKeys = {
-      "type": "onvio",
-      "sp": "onvio",
+      "type": "",
+      "sp": "",
       "uid":`${ulogin}`,
       "pwd":`${upassword}`
-    }
-    return new Promise(resolve=>{this.http.post<any>('https://demo.onvio.us/api/auth/v2/login', userKeys).subscribe(response => {
+    };
+    return new Promise(resolve=>{this.http.post<any>('url', userKeys).subscribe(response => {
       this.http.get<any>('/api',{headers: {authorization: `UDSLongToken ${response.uds}`}}).subscribe(list => {
       resolve(list.data.items);
       })
